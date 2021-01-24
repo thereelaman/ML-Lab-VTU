bbdc67eb56469dd47d555e30a178baa53f5f103d
thereelaman <amandwivedi@mail.com>
2021-01-24 17:54:22 +0530

    improved 6th program

--- 6/p6.py
+++ 6/p6.py
@@ -7,32 +7,20 @@ msg['labelnum'] = msg.label.map({'pos':1,'neg':0})
 x = msg.message
 y = msg.labelnum
 
-print(X)
-print(y)
-
 #splitting the dataset into train and test data
 from sklearn.model_selection import train_test_split
-xtrain, xtest, ytrain, ytest = train_test_split(X,y)
-
-print(xtest.shape)
-print(xtrain.shape)
-print(ytest.shape)
-print(ytrain.shape)
+xtrain, xtest, ytrain, ytest = train_test_split(x,y)
 
 #output of count vectoriser is a sparse matrix
 from sklearn.feature_extraction.text import CountVectorizer
+
 count_vect = CountVectorizer()
 xtrain_dtm = count_vect.fit_transform(xtrain)
-xtest_dtm=count_vect.transform(xtest)
-print(count_vect.get_feature_names())
-
-# df=pd.DataFrame(xtrain_dtm.toarray(),columns=count_vect.get_feature_names())
-# print(df)#tabular representation
-# print(xtrain_dtm) #sparse matrix representation
+xtest_dtm  = count_vect.transform(xtest)
 
 # Training Naive Bayes (NB) classifier on training data.
-
 from sklearn.naive_bayes import MultinomialNB
+
 clf = MultinomialNB()
 clf.fit(xtrain_dtm, ytrain)
 predicted = clf.predict(xtest_dtm)
